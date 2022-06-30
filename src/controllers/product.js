1daeec5b6b04f5d2e0bea603b802383836475473
Agung Prasetya <agungprasetya1121@gmail.com>
2022-06-30 08:13:00 +0700

    cloudinary setup

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -21,11 +21,11 @@ exports.addProduct = async (req, res) => {
 
     const { data } = req.body;
 
-    // const result = await cloudinary.uploader.upload(req.file.path, {
-    //   folder: "waysbean-agung",
-    //   use_filename: true,
-    //   unique_filename: false,
-    // });
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "waysbean-agung",
+      use_filename: true,
+      unique_filename: false,
+    });
     // code here
     let newProduct = await tb_product.create({
       ...data,
@@ -33,8 +33,7 @@ exports.addProduct = async (req, res) => {
       desc: req.body.desc,
       price: req.body.price,
       stock: req.body.stock,
-      // image: result.public_id,
-      image: req.file.filename,
+      image: result.public_id,
       idUser: req.tb_user.id,
     });
 
